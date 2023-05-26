ff6928fc49e3f5eed133e452c22dfea82e41c91d
Laszlo Sarvold <laszlo@live.com.ar>
2023-05-26 13:59:13 -0300

    docker setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+// import jsdom from 'jsdom';
+// const { JSDOM } = jsdom;
+// const dom = new JSDOM('<!doctype html><html><body></body></html>');
+// global.window = dom.window;
+// global.document = dom.window.document;
 
 test('renders learn react link', () => {
   render(<App />);
